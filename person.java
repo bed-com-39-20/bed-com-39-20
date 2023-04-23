2f93c1f5585cfe56a32b8f3458f247e862d30342
jimmy chinkango <bed-com39-20@unima.ac.mw>
2023-04-22 21:54:30 -1200

    initial commit of my project

--- person.java
+++ person.java
@@ -1,20 +1,51 @@
-class Person {
-  private String name;
-  private String address;
-  private String phoneNumber;
-  private String emailAddress;
+class Person{
+     private String name;
+     private String address;
+     private String phoneNumber;
+     private String emailAddress;
 
-  Person(String name, String address, String phoneNumber, String emailAddress) {
-    this.name = name;
-    this.address = address;
-    this.phoneNumber = phoneNumber;
-    this.emailAddress = emailAddress;
+  public Person(String name, String address, String phoneNumber, String emailAddress){
+     this.name = name;
+     this.address = address;
+     this.phoneNumber = phoneNumber;
+     this.emailAddress = emailAddress;
+  }
+  // getter and setter methods
+  // return name
+  public String getName(){
+       return name;
   }
 
-  // getter and setter methods
+  //set a new name
+  public void setName(String name){
+     this.name = name;
+  } 
+   //return address
+  public String getAddress(){
+     return address;
+  }
+    //set new address
+  public void setAddress(String address){
+     this.address = address;
+  }
 
-  //@Override
-  public String toString() {
-    return "Name: " + name;
+  // return phoneNumber
+  public String getPhoneNumber(){
+      return phoneNumber;
+  }
+  //set new phoneNumber
+  public void setphoneNumber(String phoneNumber){
+      this.phoneNumber = phoneNumber;
+  }
+   //return emailAddress
+  public String getemailAddress(){
+     return emailAddress;
+  }
+  // set new emailAdddress
+  public void setemailAddress(String emailAddress){
+      this.emailAddress = emailAddress;
+  }
+  public String toString(){
+   return "Person : " + name;
   }
 }
